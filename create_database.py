9e8420d2090b7595fa202e8872dfc84bcc58a94f
Emerson <64720988+Nosremes@users.noreply.github.com>
2022-04-06 22:14:19 -0300

    Create create_database.py

---
+++ create_database.py
@@ -0,0 +1,12 @@
+import nltk 
+from nltk.corpus import floresta
+from nltk.text import Text
+import re 
+#nltk.download("floresta")
+
+with open("database.txt","w") as f:
+    dataset = ""
+    for word,tag in floresta.tagged_words()[:1000]:
+        if tag.endswith("n")  and len(word) > 3 and not re.search(".[^a-zA-Z]+",word):
+            dataset +=  word + " "
+        f.writelines(dataset)
