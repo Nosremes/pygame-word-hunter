e13d033a5d0c9548d7c1ee2727e72b35712b96c3
Emerson <64720988+Nosremes@users.noreply.github.com>
2022-04-07 13:38:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,22 @@ import random
 
 colors = {
     "white":(255,255,255),"black":(0,0,0),
-    "red":(255,0,0),"green":(0,255,0),"blue":(0,0,255)}
+    "red":(255,0,0),"green":(0,255,0),"blue":(0,0,255),
+    "pink":(255, 0, 214)}
 
-words_teste = [
+
+words = [
     "amor","cachorro","cama","luz",
     "armario","sofa"]
 
+"""
 with open("database.txt") as f:
     words = f.readline().split(" ")
-    #print(words)
+"""
 
 pygame.init()
-x,y = (800,600)
-window = pygame.display.set_mode((x,y))
+width,height = (800,600)
+window = pygame.display.set_mode((width,height))
 pygame.display.set_caption("Word hunter")
 
 class DrawMainWord:
@@ -32,7 +35,10 @@ class DrawMainWord:
         self.font = pygame.font.SysFont("Arial",100,True)
         self.update_word()
 
-    def draw(self,window):
+    def draw(self,window:pygame.Surface) -> None:
+        """
+            Blit the word surface on the given surface
+        """
         if self.win:
             surface = self.font.render(self.word,True,colors["black"])
             self.margin_x = 800 / 2 - surface.get_width()/2
@@ -46,7 +52,10 @@ class DrawMainWord:
             window.blit(surface,(self.margin_x,self.margin_y))
 
 
-    def update_word(self):
+    def update_word(self) -> None:
+        """
+            randomly choose a word from self.all_words 
+        """
         new_word = random.choice(self.all_words)
         if new_word != self.word:
             self.word = new_word
@@ -56,14 +65,19 @@ class DrawMainWord:
         else:
             self.update_word()
 
-    def try_letter(self,letter):
+    def try_letter(self,letter:str) -> bool:
+        """
+            check if is the right letter 
+        """
+        global points
         if letter.upper() == self.word[self.letter_index].upper():
-            global points
             if not self.win:
                 points += 1
             self.win = True
             return True
-        return False
+        else:
+            points -= 1
+            return False
 
 class Letters:
     ALPHABET = [
@@ -72,7 +86,7 @@ class Letters:
                 "O","P","Q","R","S","T","U",
                 "V","W","X","Y","Z"]
     
-    def __init__(self,word):
+    def __init__(self,word:list):
         self.alphabet = type(self).ALPHABET.copy()
         self.wrongs = []
         self.word = word
@@ -80,29 +94,25 @@ class Letters:
         self.all_surfaces = [
                 self.font.render(x,True,colors["black"])for x in self.alphabet]
         self.rects = []
-        x = 100
-        y = 300
-        space_between = 20
-        for surface in self.all_surfaces:
-            rect = surface.get_rect()
-            if (x + surface.get_width() + space_between) > 700:
-                y += 100
-                x = 100
-            rect.x = x 
-            rect.y = y
-            x += surface.get_width() + space_between
-            self.rects.append(rect)
+        self.update_alpha()
 
-    def draw(self,window):
+    def draw(self,window:pygame.Surface) -> None:
+        """
+            Blit the letters surface on the given surface 
+        """
         for i,surface in enumerate(self.all_surfaces):
             window.blit(surface,self.rects[i])
             mouse_pos = pygame.mouse.get_pos()
             if self.rects[i].collidepoint(mouse_pos):
                 rect = self.rects[i]
                 rect = pygame.Rect.inflate(rect,15,10)
-                pygame.draw.rect(window,colors["blue"],rect,2)
+                pygame.draw.rect(window,colors["pink"],rect,2)
     
-    def track_inputs(self,event):
+    def track_inputs(self,event:list) -> None:
+        """
+            check if clicked on some letter
+            if wrong letter, remove it 
+        """
         if event.type == MOUSEBUTTONDOWN:
             for i,rect in enumerate(self.rects):
                 if rect.collidepoint(event.pos):
@@ -113,11 +123,15 @@ class Letters:
                         points -= 1
                         self.alphabet.pop(i)
                         self.update_alpha()
-    def update_alpha(self):
+    def update_alpha(self) -> None:
+        """
+            update list of surfaces and rectangles 
+            to sync with remaining letters 
+        """
         self.all_surfaces = [
                 self.font.render(x,True,colors["black"]) for x in self.alphabet]
         x = 100
-        y = 300
+        y = 340
         space_between = 20
         self.rects = [] 
         for surface in self.all_surfaces:
@@ -131,12 +145,19 @@ class Letters:
             x += surface.get_width() + space_between
             self.rects.append(rect)
     
-    def reset_alpha(self):
+    def reset_alpha(self) -> None:
+        """
+            get all letters again
+        """
         self.alphabet = type(self).ALPHABET.copy()
         self.update_alpha()
     
 word = DrawMainWord(100,words)
 letters_buttons = Letters(word)
+next_button = pygame.image.load("button.png")
+rect_next_button = next_button.get_rect()
+rect_next_button.x = 363
+rect_next_button.y = 240
 points = 0
 font = pygame.font.SysFont("Arial",20,True,True)
 
@@ -146,10 +167,8 @@ while True:
     letters_buttons.draw(window)
     points_surface = font.render("POINTS: " + str(points),True,colors["black"])
     window.blit(points_surface,(20,20))
+    window.blit(next_button,(rect_next_button))
     for event in pygame.event.get():
-        # track mouse
-        letters_buttons.track_inputs(event)
-        
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
@@ -157,8 +176,13 @@ while True:
             if event.key == K_SPACE:
                 word.update_word()
                 letters_buttons.reset_alpha()
-            #try key typed
-            word.try_letter(pygame.key.name(event.key))
+            else:
+                word.try_letter(pygame.key.name(event.key))
+        if event.type == MOUSEBUTTONDOWN:
+            if rect_next_button.collidepoint(event.pos):
+                word.update_word()
+                letters_buttons.reset_alpha()
+        letters_buttons.track_inputs(event)
 
     pygame.display.update()    
 
